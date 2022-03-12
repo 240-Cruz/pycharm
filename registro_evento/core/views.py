0d1fb068e14ac17ad40614c2d49df4549b202b43
240-Cruz <83036343+240-Cruz@users.noreply.github.com>
2022-03-11 23:06:32 -0500

    Update views.py

--- registro_evento/core/views.py
+++ registro_evento/core/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request):
     template_name="index.html"
     return render(request, template_name)
+
+def quienes(request):
+    template_name="quienes.html"
+    return render(request, template_name)
+
