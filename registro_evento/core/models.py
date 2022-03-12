3566258c456e78cfb99bbce52f366f04e1225d5c
240-Cruz <83036343+240-Cruz@users.noreply.github.com>
2022-03-11 23:49:24 -0500

    Update models.py

--- registro_evento/core/models.py
+++ registro_evento/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class register(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    num_doc = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    date = models.DateField()
+    coments = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name +"-" + self.last_name
+
+
